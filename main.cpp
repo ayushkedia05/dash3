50ce8d586b74f32182d8f615fc9c5a12c40bccc5
Ayush <kediaayush26@gmail.com>
2022-05-13 15:31:21 +0530

    3

--- main.cpp
+++ main.cpp
@@ -1,62 +1,64 @@
-      #include <bits/stdc++.h>
-      using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-      #define int long long
-      long long binpow(long long a, long long b)
-      {
-        if (b == 0)
-          return 1;
-        long long res = binpow(a, b / 2);
-        if (b % 2)
-          return res * res * a;
-        else
-          return res * res;
-      }
+#define int long long
+long long binpow(long long a, long long b)
+{
+  if (b == 0)
+    return 1;
+  long long res = binpow(a, b / 2);
+  if (b % 2)
+    return res * res * a;
+  else
+    return res * res;
+}
 
-        unsigned highestPowerof2(unsigned x)
-        {
+unsigned highestPowerof2(unsigned x)
+{
 
-        x |= x >> 1;
-        x |= x >> 2;
-        x |= x >> 4;  
-        x |= x >> 8;
-        x |= x >> 16;
+  x |= x >> 1;
+  x |= x >> 2;
+  x |= x >> 4;
+  x |= x >> 8;
+  x |= x >> 16;
 
-        return x ^ (x >> 1);
-      }
-    
-    int gcd(int a, int b)
+  return x ^ (x >> 1);
+}
+
+int gcd(int a, int b)
+{
+  return b == 0 ? a : gcd(b, a % b);
+}
+
+signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n,k;
+  cin >> n>>k;
+  vector<int> vec,dp(k+1,0);
+  for(int i=0;i<n;i++)
   {
-      return b == 0 ? a : gcd(b, a % b);   
+    int a;
+    cin>>a;
+    vec.push_back(a);
   }
+  dp[0]=1 ;
+  sort(vec.begin(),vec.end());
+  for(int i=0;i<n;i++)
+  {
+    for(int j=1;j<=k;j++)
+    {
+      if(j-vec[i]<0)
+      break;  
+      dp[j]=dp[j]+dp[j-vec[i]];
+    }
+  }
+ if(dp[k]==1e9)
+ cout<<-1;
+ else
+  cout<<dp[k];
 
-      signed main()
-      {
-    ios_base::sync_with_stdio(false);
-      cin.tie(NULL);
-        int t;
-        cin>>t;
-        while (t--)
-        {
-int a,b,c;
-  cin>>a>>b>>c;
-
-   a=pow(10,a-c);
-  b=pow(10,b-c)-1;
-  if(b==0)
-  b++;
-
-   c=pow(10,c)-1;
-
-  cout<<c*a<<" "<<c*b;
-
-
-          cout<<"\n";
-        }
-                
-          
-
-        
-        
-        return 0;
-      }
+  return 0;
+}
